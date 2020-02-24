88d0ea08e30c728e1e8dc6b97c5e3822205ff93d
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-02-24 16:23:48 +0100

    added constructor

--- drawit/src/drawit/DoublePoint.java
+++ drawit/src/drawit/DoublePoint.java
@@ -1,5 +1,25 @@
 package drawit;
 
+class Node {
+	int double_x;
+	int double_y;
+}
+
 public class DoublePoint {
+	public DoublePoint(int x ,int y) {
+		double_x = x;
+		double_y = y;
+		
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
