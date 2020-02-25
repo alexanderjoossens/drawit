ba51f2d12e914fc7279eb8079ec9d96e3107ac00
alexander <alexanderjoossens@gmail.com>
2020-02-25 21:14:58 +0100

    begonnen aan PointArrays

--- drawit/src/drawit/PointArrays.java
+++ drawit/src/drawit/PointArrays.java
@@ -1,5 +1,47 @@
 package drawit;
 
 public class PointArrays {
+	
+	
+	/**
+	 * Returns null if the given array of points defines a proper polygon; otherwise, returns a string describing why it does not.
+	 * @param points
+	 * @param index
+	 * @param value
+	 * @return
+	 */
 
+	public static String checkDefinesProperPolygon(IntPoint[] points)
+	
+	
+	
+	
+	public static IntPoint[] copy​(IntPoint[] points)
+	
+	
+	IntPoint[] newarray = new IntPoint[points.length()]
+			
+	
+			
+	public static IntPoint[] insert​(IntPoint[] points,
+			int index,
+			IntPoint point)
+	
+	IntPoint[] newarray = new IntPoint[points.length()]
+	newarray[points[int index, IntPoint point]]
+	
+	
+	
+	public static IntPoint[] remove​(IntPoint[] points,
+			int index)
+	
+	
+	
+
+	public static IntPoint[] update​(IntPoint[] points,
+			int index,
+			IntPoint value)
+	
+	
+	
 }
