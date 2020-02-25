e1434396bfedd22357922c717cd9c4ed7316569c
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-02-25 20:15:31 +0100

    IntPoint intersect gemaakt

--- drawit/src/drawit/IntPoint.java
+++ drawit/src/drawit/IntPoint.java
@@ -3,51 +3,58 @@ package drawit;
 public class IntPoint {
 	private int x;
 	private int y;
-	
+
 	public IntPoint(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public int getX() {
 		return this.x;
 	}
-	
+
 	public int getY() {
 		return this.y;
 	}
-	
+
 	boolean equals(IntPoint other) {
 		if (this.x == other.getX() && this.y == other.getY()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	DoublePoint asDoublePoint() {
 		DoublePoint point = new DoublePoint(this.x, this.y);
 		return point;
 	}
-	
+
 	boolean isOnLineSegment(IntPoint a, IntPoint b) {
-		double rico = (b.getY()-a.getY()) / (b.getX()-a.getX());
-		double function = rico*(b.getX()-this.x) + this.y;
+		double rico = (b.getY() - a.getY()) / (b.getX() - a.getX());
+		double function = rico * (b.getX() - this.x) + this.y;
 		if (function == b.getY()) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
-		}	
+		}
+	}
+	
+	static boolean isCounterClockWise(IntPoint a, IntPoint b, IntPoint c) {
+		return (c.getY()-a.getY()) * (b.getX()-a.getX()) > (b.getY()-a.getY()) * (c.getX()-a.getX());
+		
 	}
 	
 	static boolean lineSegmentsIntersect(IntPoint a, IntPoint b, IntPoint c, IntPoint d) {
-		double rico1 = (b.getY()-a.getY()) / (b.getX()-a.getX());
-		double rico2 = (b.getY()-a.getY()) / (b.getX()-a.getX());
+//		double rico1 = (b.getY() - a.getY()) / (b.getX() - a.getX());
+//		double rico2 = (d.getY() - c.getY()) / (d.getX() - c.getX());
+//
+//		double xIntersection = (b.getY() - d.getY() + rico2 * d.getX() - rico1 * b.getX()) / (rico2 - rico1);
+//		double yIntersection = rico2 * (xIntersection - d.getX()) + d.getY();
 		
-				
-		return true;
+		return isCounterClockWise(a,c,d) != isCounterClockWise(b,c,d) && isCounterClockWise(a,b,c) != isCounterClockWise(a,b,d);
 	}
+	
+	
 
 }
