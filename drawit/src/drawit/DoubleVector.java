ce8769654c81b7ce30a6b5541dee3718ee64d260
alexander <alexanderjoossens@gmail.com>
2020-02-25 18:56:06 +0100

    Ik ben begonnen met wat documentaties voor de methods van DoubleVector

--- drawit/src/drawit/DoubleVector.java
+++ drawit/src/drawit/DoubleVector.java
@@ -10,7 +10,6 @@ public class DoubleVector {
         this.y = y;
     }
 
-    
     public double getX() {
     	return this.x;
     }
@@ -19,15 +18,35 @@ public class DoubleVector {
     	return this.y;
     }
     
+    /**
+     * Returns the angle from positive X to this vector, in radians. The angle from positive X to positive Y is Math.PI / 2; the angle from positive X to negative Y is -Math.PI / 2.
+     * @return
+     * 
+     * @post The result ...
+     * 
+     */
+    
     public double asAngle() {
     	return Math.atan(this.y/this.x);
     	
     }
-
+    /**
+     * Returns the cross product of this vector and the given vector.
+     * 
+     * @post result == this.getX() * other.getY() - this.getY() * other.getX()
+     * 
+     */
     public double crossProduct(DoubleVector other) {
     	double crossProd = (this.x * other.y) - (this.y * other.x);
     	return crossProd;
     }
+    
+    /**
+     * Returns the dot product of this vector and the given vector.
+     * @param other
+     * @return
+     * @post result == this.getX() * other.getX() + this.getY() * other.getY()
+     */
 
     public double dotProduct(DoubleVector other) {
     	double dotProd = (this.x * other.x) + (this.y * other.y);
