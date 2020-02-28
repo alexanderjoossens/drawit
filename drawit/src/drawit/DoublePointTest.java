11cee5da23d30c800f44293da94dcfeda44bc106
alexander <alexanderjoossens@gmail.com>
2020-02-28 16:34:22 +0100

    Test cases in source folder gecreate

---
+++ drawit/src/drawit/DoublePointTest.java
@@ -0,0 +1,14 @@
+package drawit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DoublePointTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
