a56f03e9e6a0184a4175495b8bd2f77da16e5232
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-02-28 16:57:12 +0100

    alles is nu gefixt

--- drawit/src/drawit/DoublePointTest.java
+++ drawit/src/drawit/DoublePointTest.java
@@ -8,7 +8,14 @@ class DoublePointTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		DoublePoint point = new DoublePoint(4.4 , 3.4);
+//		assert point.getX() == 4;
+		
+		IntPoint point2 = new DoublePoint(4 , 3).round();
+		
+		assert point2.getX() == 4;
+		
+		
 	}
 
 }
