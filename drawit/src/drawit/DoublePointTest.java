253948b29efac3e64e57dc72cc1581cec3e77160
alexander <alexanderjoossens@gmail.com>
2020-02-28 17:08:52 +0100

    nieuwe test voor DoublePoint

--- drawit/src/drawit/DoublePointTest.java
+++ drawit/src/drawit/DoublePointTest.java
@@ -8,12 +8,19 @@ class DoublePointTest {
 
 	@Test
 	void test() {
-		DoublePoint point = new DoublePoint(4.4 , 3.4);
-//		assert point.getX() == 4;
+		DoublePoint point1= new DoublePoint(4.4 , 3.4);
+		assert point1.getX() == 4.4;
 		
-		IntPoint point2 = new DoublePoint(4 , 3).round();
+		IntPoint point2 = point1.round();
 		
 		assert point2.getX() == 4;
+		
+		DoublePoint point3 = new DoublePoint(3.5, -1.5);
+		assert point3.getY() == -1.5;
+		IntPoint point4 = point3.round();
+		assert point4.getX() == 4 && point4.getY() == -1;
+		
+		
 		
 		
 	}
