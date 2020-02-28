ad0e3ae0764d31bed3ee087cb8137186d4e2123f
alexander <alexanderjoossens@gmail.com>
2020-02-28 18:15:07 +0100

    DoubleVectorTest half af

--- drawit/src/drawit/DoubleVectorTest.java
+++ drawit/src/drawit/DoubleVectorTest.java
@@ -8,7 +8,18 @@ class DoubleVectorTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		DoubleVector point1 = new DoubleVector(2.1, 2.1);
+		assert point1.getX() == 2.1 && point1.getY() == 2.1;
+		assert point1.asAngle() == Math.PI/4;
+		
+		DoubleVector point2 = new DoubleVector(2, 3);
+		double x = point1.crossProduct(point2);
+		//System.out.println(p);
+		assert x == 2.1000000000000005; //wat doen we hiermee?
+		
+		x = point1.dotProduct(point2);
+		assert x == 10.5;
+		
+		
 	}
-
 }
