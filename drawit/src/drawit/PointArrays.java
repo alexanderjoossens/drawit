565b3a1b88fcafed78ce69e07ef0401b61116e14
Stefan <Stefan@DESKTOP-MI4LFJR>
2020-02-29 12:49:30 +0100

    PointArrays bugfix + PointArrays test af!

--- drawit/src/drawit/PointArrays.java
+++ drawit/src/drawit/PointArrays.java
@@ -16,21 +16,18 @@ public class PointArrays {
 	}
 
 	public static IntPoint[] insert(IntPoint[] points, int index, IntPoint point) {
-		IntPoint[] newArray = new IntPoint[points.length+1];
+		IntPoint[] newArray = new IntPoint[points.length + 1];
 		int pointsIndex = 0;
-		for (int i = 0; i < points.length+1; i++ ) {
+		for (int i = 0; i < points.length + 1; i++) {
 			if (i == index) {
 				newArray[i] = point;
+			} else {
+				newArray[i] = points[pointsIndex];
 				pointsIndex++;
 			}
-			else {
-				newArray[i] = points[pointsIndex];
-		
-			}
-			pointsIndex++;
 		}
 		return newArray;
-		
+
 	}
 
 	public static IntPoint[] remove(IntPoint[] points, int index) {
@@ -68,40 +65,40 @@ public class PointArrays {
 	 * @param value
 	 * @return
 	 */
-	
+
 	static String checkDefinesProperPolygon(IntPoint[] points) {
-		for (int i=0; i<points.length;i++) {
-			for (int j = 0; j<points.length; j++) {
-				if (points[i].equals(points[j]) && i!=j) {
+		for (int i = 0; i < points.length; i++) {
+			for (int j = 0; j < points.length; j++) {
+				if (points[i].equals(points[j]) && i != j) {
 					return "2 vertices coincide!";
 				}
 			}
 		}
-		if (points[0].isOnLineSegment(points[1], points[points.length-1])) {
+		if (points[0].isOnLineSegment(points[1], points[points.length - 1])) {
 			return "There is a vertex on an edge!";
 		}
-		if (points[points.length-1].isOnLineSegment(points[points.length-2], points[0])) {
+		if (points[points.length - 1].isOnLineSegment(points[points.length - 2], points[0])) {
 			return "There is a vertex on an edge!";
 		}
-		for (int i=1; i<points.length-1;i++) {
-			if (points[i].isOnLineSegment(points[i-1], points[i+1])) {
+		for (int i = 1; i < points.length - 1; i++) {
+			if (points[i].isOnLineSegment(points[i - 1], points[i + 1])) {
 				return "There is a vertex on an edge!";
 			}
 		}
-		
-		for (int i = 0; i<points.length-1; i++) {
-			for (int j = 0; j<points.length-1;i++) {
-				if (IntPoint.lineSegmentsIntersect(points[i], points[i+1], points[j], points[j+1]) && i!=j) {
-					return ("Lines intersect!"+i+j);
+
+		for (int i = 0; i < points.length - 1; i++) {
+			for (int j = 0; j < points.length - 1; j++) {
+				if (IntPoint.lineSegmentsIntersect(points[i], points[i + 1], points[j], points[j + 1]) && i != j) {
+					return ("Lines intersect!");
 				}
 			}
 		}
-		for (int i= 0; i<points.length-1;i++) {
-			if (IntPoint.lineSegmentsIntersect(points[i], points[i+1], points[0], points[points.length-1])) {
+		for (int i = 0; i < points.length - 1; i++) {
+			if (IntPoint.lineSegmentsIntersect(points[i], points[i + 1], points[0], points[points.length - 1])) {
 				return "Lines intersect!";
 			}
 		}
-					
+
 		return null;
 	}
 
--- drawit/src/drawit/PointArraysTest.java
+++ drawit/src/drawit/PointArraysTest.java
@@ -8,34 +8,75 @@ class PointArraysTest {
 
 	@Test
 	void test() {
-		IntPoint point1 = new IntPoint(1,1);
-		IntPoint point2 = new IntPoint(3,2);
-		IntPoint point3 = new IntPoint(2,3);
-		IntPoint point4 = new IntPoint(1,3);
-		
-		IntPoint[] pointArray= new IntPoint[4];
-		
+		IntPoint point1 = new IntPoint(1, 1);
+		IntPoint point2 = new IntPoint(3, 2);
+		IntPoint point3 = new IntPoint(2, 3);
+		IntPoint point4 = new IntPoint(1, 3);
+		IntPoint point5 = new IntPoint(0, -2);
+		IntPoint point6 = new IntPoint(0, 0);
+		IntPoint point7 = new IntPoint(0, 2);
+		IntPoint point8 = new IntPoint(2, 2);
+		IntPoint point9 = new IntPoint(2, 0);
+		IntPoint point10 = new IntPoint(2, 0);
+		IntPoint point11 = new IntPoint(1, 1);
+
+		IntPoint[] pointArray = new IntPoint[4];
+
 		pointArray[0] = point1;
 		pointArray[1] = point2;
 		pointArray[2] = point3;
 		pointArray[3] = point4;
-		
+
+		IntPoint[] pointArray6 = new IntPoint[4];
+
+		pointArray6[0] = point6;
+		pointArray6[1] = point7;
+		pointArray6[2] = point8;
+		pointArray6[3] = point9;
+
+		IntPoint[] pointArray7 = new IntPoint[4];
+
+		pointArray7[0] = point7;
+		pointArray7[1] = point8;
+		pointArray7[2] = point9;
+		pointArray7[3] = point10;
+
+		IntPoint[] pointArray8 = PointArrays.update(pointArray7, 3, point11);
+
+		// copy_Test
 		IntPoint[] pointArray2 = PointArrays.copy(pointArray);
-		
+
 		assert pointArray[0].getX() == 1;
 		assert pointArray2[0].getX() == 1;
-		System.out.println(PointArrays.checkDefinesProperPolygon(pointArray));
-		assert PointArrays.checkDefinesProperPolygon(pointArray) == null;
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		assert pointArray2[3].getY() == 3;
 
+		// insert_Test
+		IntPoint[] pointArray3 = PointArrays.insert(pointArray, 2, point5);
+
+		assert pointArray3.length == 5;
+		assert pointArray3[2] == point5;
+		assert pointArray3[3] == point3;
+
+		// remove_Test
+		IntPoint[] pointArray4 = PointArrays.remove(pointArray3, 2);
+		assert pointArray4.length == 4;
+		assert pointArray4[2] == point3;
+		assert pointArray4[3] == point4;
+
+		// update_Test
+		IntPoint[] pointArray5 = PointArrays.update(pointArray, 2, point5);
+		assert pointArray5.length == 4;
+		assert pointArray5[1] == point2;
+		assert pointArray5[2] == point5;
+		assert pointArray5[3] == point4;
+
+		// checkDefinesProperPolygon_Test
+		assert PointArrays.checkDefinesProperPolygon(pointArray) == null;
+		assert PointArrays.checkDefinesProperPolygon(pointArray6) == null;
+		assert PointArrays.checkDefinesProperPolygon(pointArray3) == "Lines intersect!";
+		assert PointArrays.checkDefinesProperPolygon(pointArray7) == "2 vertices coincide!";
+		assert PointArrays.checkDefinesProperPolygon(pointArray8) == "There is a vertex on an edge!";
+		
 	}
 
 }
