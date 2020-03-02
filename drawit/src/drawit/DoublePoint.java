395eeb6f4edcb7219f16d52e84cb95c87e932c81
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-03-02 11:56:45 +0100

    private final geplaatst waar nodig, en @immutable gezet

--- drawit/src/drawit/DoublePoint.java
+++ drawit/src/drawit/DoublePoint.java
@@ -1,67 +1,46 @@
 package drawit;
 
+/**
+ * @immutable
+ */
 public class DoublePoint {
-	
-	private double x;
-	private double y;
-	
+
+	private final double x;
+	private final double y;
+
 	public DoublePoint(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
-	/**
-	 * Returns this point's X coordinate.
-	 * @return
-	 */
+
 	public double getX() {
 		return this.x;
 	}
-	
-	/**
-	 * Returns this point's Y coordinate.
-	 * @return
-	 */
+
 	public double getY() {
 		return this.y;
 	}
-	
-	/**
-	 * Returns the point that you become after adding the given vector to the given point (=sum).
-	 * @param other
-	 * @return
-	 */
+
 	public DoublePoint plus(DoubleVector other) {
 		double xCoord = this.x + other.getX();
 		double yCoord = this.y + other.getY();
 		DoublePoint point = new DoublePoint(xCoord, yCoord);
 		return point;
 	}
-	
-	/**
-	 * Returns the point that you become after distracting the given vector from the given point (=difference).
-	 * @param other
-	 * @return
-	 */
+
 	public DoubleVector minus(DoublePoint other) {
 		double xCoord = this.x - other.getX();
 		double yCoord = this.y - other.getY();
 		DoubleVector vector = new DoubleVector(xCoord, yCoord);
-		return vector;	
+		return vector;
 	}
-	
-	
-	/**
-	 * Returns an IntPoint object whose coordinates are obtained by rounding the coordinates of this point to the nearest integer.
-	 * @return
-	 */
+
 	public IntPoint round() {
 		int xCoord = (int) Math.round(this.x);
 		int yCoord = (int) Math.round(this.y);
 		IntPoint point = new IntPoint(xCoord, yCoord);
 		return point;
-		
-		
+
 	}
 
 }
--- drawit/src/drawit/DoubleVector.java
+++ drawit/src/drawit/DoubleVector.java
@@ -1,97 +1,56 @@
 package drawit;
 
+/**
+ * @immutable
+ */
 public class DoubleVector {
-	
-    double x;
-    double y;
 
-    public DoubleVector(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+	private final double x;
+	private final double y;
 
-    /**
-     * Returns the X value of the given vector.
-     * @return
-     */
-    public double getX() {
-    	return this.x;
-    }
-    
-    /**
-     * Returns the Y value of the given vector.
-     * @return
-     */
-    public double getY() {
-    	return this.y;
-    }
-    
-    /**
-     * Returns the angle from positive X to this vector, in radians.
-     * The angle from positive X to positive Y is Math.PI / 2; the angle from positive X to negative Y is -Math.PI / 2.
-     * 
-     * @return
-     * 
-     * @post The result ...
-     * 
-     */
-    public double asAngle() {
-    	return Math.atan2(this.y, this.x);
-    	
-    }
-    /**
-     * Returns the cross product of this vector and the given vector.
-     * 
-     * @post result == this.getX() * other.getY() - this.getY() * other.getX()
-     * 
-     */
-    public double crossProduct(DoubleVector other) {
-    	double crossProd = (this.x * other.y) - (this.y * other.x);
-    	return crossProd;
-    }
-    
-    /**
-     * Returns the dot product of this vector and the given vector.
-     * 
-     * @param other
-     * @return
-     * @post result == this.getX() * other.getX() + this.getY() * other.getY()
-     */
-    public double dotProduct(DoubleVector other) {
-    	double dotProd = (this.x * other.x) + (this.y * other.y);
-    	return dotProd;
+	public DoubleVector(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
-    }
-    /**
-     * Returns the size (=norm) of the given vector.
-     * 
-     * @return
-     */
-    public double getSize() {
-    	double size = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-    	return size;
-    }
-    
-    /**
-     * Returns the vector that you become after adding the given vector to the other given vector.
-     * 
-     * @param other
-     * @return
-     */
-    public DoubleVector plus(DoubleVector other) {
-    	double xCoord = other.x;
-    	double yCoord = other.y;
-    	DoubleVector vector = new DoubleVector(xCoord+this.x, yCoord+this.y);
-    	return vector;
-    }
-    /**
-     * Returns a DoubleVector object whose coordinates are obtained by multiplying this vector's coordinates by the given scale factor.
-     * @param 
-     * @return
-     */
-    public DoubleVector scale(double d) {
-    	DoubleVector vector = new DoubleVector(this.x*d, this.y*d);
-    	return vector;
-    }
+	public double getX() {
+		return this.x;
+	}
+
+	public double getY() {
+		return this.y;
+	}
+
+	public double asAngle() {
+		return Math.atan2(this.y, this.x);
+
+	}
+
+	public double crossProduct(DoubleVector other) {
+		double crossProd = (this.x * other.y) - (this.y * other.x);
+		return crossProd;
+	}
+
+	public double dotProduct(DoubleVector other) {
+		double dotProd = (this.x * other.x) + (this.y * other.y);
+		return dotProd;
+
+	}
+
+	public double getSize() {
+		double size = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+		return size;
+	}
+
+	public DoubleVector plus(DoubleVector other) {
+		double xCoord = other.x;
+		double yCoord = other.y;
+		DoubleVector vector = new DoubleVector(xCoord + this.x, yCoord + this.y);
+		return vector;
+	}
+
+	public DoubleVector scale(double d) {
+		DoubleVector vector = new DoubleVector(this.x * d, this.y * d);
+		return vector;
+	}
 }
-
