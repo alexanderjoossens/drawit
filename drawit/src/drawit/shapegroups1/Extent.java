37f9c070e76896db8dd00618917ee14ed5fd74d3
alexander <alexanderjoossens@gmail.com>
2020-03-22 10:51:50 +0100

    methods Extent1

--- drawit/src/drawit/shapegroups1/Extent.java
+++ drawit/src/drawit/shapegroups1/Extent.java
@@ -1,20 +1,39 @@
 package drawit.shapegroups1;
-package drawit //waarom kan ik dit niet importen?? ik denk dat de drawit folder een ander type folder/package is want op github staan die methods ook niet in een mapje maar gwn onder elkaar hier https://github.com/stefanlommaert/drawit/tree/master/drawit/src/drawit
+package drawit
+
+import drawit.IntPoint;//waarom kan ik dit niet importen?? ik denk dat de drawit folder een ander type folder/package is want op github staan die methods ook niet in een mapje maar gwn onder elkaar hier https://github.com/stefanlommaert/drawit/tree/master/drawit/src/drawit
 
 public class Extent {
 	
+	private final int X1;
+	private final int Y1;
+	private final int X2;
+	private final int Y2;
+	private final int X3;
+	private final int Y3;
+	private final int X4;
+	private final int Y4;
+	
 	/**
 	 * 
-	 * @param X1
-	 * @param Y1
-	 * @param X2
-	 * @param Y2
-	 * @param X3
-	 * @param Y3
-	 * @param X4
-	 * @param Y4
+	 * @param X1 The X coordinate of the left under point.
+	 * @param Y1 The Y coordinate of the left under point.
+	 * @param X2 The X coordinate of the right under point.
+	 * @param Y2 The Y coordinate of the right under point.
+	 * @param X3 The X coordinate of the top right point.
+	 * @param Y3 The Y coordinate of the top right point.
+	 * @param X4 The X coordinate of the top left point.
+	 * @param Y4 The Y coordinate of the top left point.
 	 */
-	public Extent(double X1, double Y1, double X2, double Y2, double X3, double Y3, double X4, double Y4) {
+	public Extent(int X1, int Y1, int X2, int Y2, int X3, int Y3, int X4, int Y4) {
+		this.X1 = X1;
+		this.Y1 = Y1;
+		this.X2 = X2;
+		this.Y2 = Y2;
+		this.X3 = X3;
+		this.Y3 = Y3;
+		this.X4 = X4;
+		this.Y4 = Y4;
 	}
 
 	/**
@@ -32,34 +51,90 @@ public class Extent {
 	 * @return
 	 */
 	public int getBottom() {
-		
+		return Y3;
 	}
 	
 	public IntPoint getBottomRIght() {
-		
+		IntPoint[] newArray = new IntPoint[4];
+		return newArray;
 	}
 	
+	/**
+	 * Returns the distance between the edges that are parallel to the X axis.
+	 * @return
+	 */
 	public int getHeight() {
-		
+		return Y4-Y1;
 	}
 	
+	/**
+	 * Returns the X coordinate of the edge parallel to the Y axis with the smallest X coordinate.
+	 * @return
+	 */
 	public int getLeft() {
-		
+		return X1;
 	}
 	
+	/**
+	 * Returns the X coordinate of the edge parallel to the Y axis with the largest X coordinate.
+	 * @return
+	 */
 	public int getRight() {
-		
+		return X2;
 	}
 	
+	/**
+	 * Returns the Y coordinate of the edge parallel to the X axis with the smallest Y coordinate.
+	 * @return
+	 */
 	public int getTop() {
-		
+		return Y1;
 	}
 	
 	public IntPoint getTopLeft() {
+		IntPoint[] newArray = new IntPoint[4];
+		return newArray;
+	}
+	
+	/**
+	 * 	
+Returns the distance between the edges that are parallel to the Y axis.
+	 * @return
+	 */
+	public int getWidth() {
+		return X2-X1;
+	}
+	
+	public static Extent ofLeftTopRightBottom(int left, int top; int width, int bottom) {
 		
 	}
 	
-	public int getWidth() {
+	
+	public static Extent ofLeftTopWidthHeight(int left, int top; int width, int bottom) {
+		
+	}
+	
+	public Extent withBottom(int newBottom) {
+		
+	}
+	
+	public Extent withHeight(int NewHeight) {
+		
+	}
+	
+	public Extent withLeft(int newLeft) {
+		
+	}
+	
+	public Extent withRight(int newRight) {
+		
+	}
+	
+	public Extent withTop(int newTop) {
+		
+	}
+	
+	public Extent withWidth(int newWidth) {
 		
 	}
 	
