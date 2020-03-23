17a9bb5341f5de07a16b719cb0e6cb85595afc73
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-03-23 14:05:33 +0100

    constructor gemaakt

--- drawit/src/drawit/shapegroups2/ShapeGroup.java
+++ drawit/src/drawit/shapegroups2/ShapeGroup.java
@@ -1,5 +1,13 @@
 package drawit.shapegroups2;
-
+import drawit.RoundedPolygon;
 public class ShapeGroup {
-
+	
+	
+	public ShapeGroup(RoundedPolygon shape) {
+		
+	}
+	
+	public ShapeGroup(ShapeGroup[] subgroups) {
+		
+	}
 }
