a42c82b886bec448bfebd7771fc7a9ddf2257e20
alexander <alexanderjoossens@gmail.com>
2020-03-28 15:17:30 +0100

    shapegroup1 constructors

--- drawit/src/drawit/shapegroups1/ShapeGroup.java
+++ drawit/src/drawit/shapegroups1/ShapeGroup.java
@@ -1,5 +1,23 @@
 package drawit.shapegroups1;
 
+import drawit.RoundedPolygon;
+
 public class ShapeGroup {
+	
+	/**
+	 * Initializes this object to represent a leaf shape group that directly contains the given shape.
+	 * @param shape
+	 */
+	public void ShapeGroup​(RoundedPolygon shape) {
+		
+	}
+	
+	/**
+	 * Initializes this object to represent a non-leaf shape group that directly contains the given subgroups, in the given order.
+	 * @param subgroups
+	 */
+	public void ShapeGroup​(ShapeGroup[] subgroups) {
+		
+	}
 
 }
