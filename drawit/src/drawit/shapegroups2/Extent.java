c0133e43584ffc88927be6c20154cfe28b68a191
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-03-29 17:12:51 +0200

    extent2 opnieuw gemaakt, zou nu helemaal juist moeten zijn hoop ik

--- drawit/src/drawit/shapegroups2/Extent.java
+++ drawit/src/drawit/shapegroups2/Extent.java
@@ -1,173 +1,190 @@
 package drawit.shapegroups2;
 
-import drawit.IntPoint;		
-
-// moet er wel overal this. voor bij laatste methods? nog vragen in forum (reminder)
-// moet er wel documentatie in deze class? in html file staat van niet
+import drawit.IntPoint;
+import drawit.RoundedPolygon;
 
 public class Extent {
-	
-	private final int left;
-	private final int top;
-	private final int width;
-	private final int height;
 
-	/**
-	 * 
-	 * @param left
-	 * @param top
-	 * @param width
-	 * @param height
-	 */
-	public Extent(int left, int top, int width, int height) {
-		this.left = left;
-		this.top = top;
-		this.width = width;
-		this.height = height;
+	private int left;
+	private int top;
+	private int width;
+	private int height;
+
+	public static Extent ofLeftTopRightBottom(int left, int top, int right, int bottom) {
+		Extent extent = new Extent();
+		extent.left = left;
+		extent.top = top;
+		extent.width = right - left;
+		extent.height = bottom - top;
+		return extent;
+	}
+
+	public static Extent ofLeftTopWidthHeight(int left, int top, int width, int height) {
+		Extent extent = new Extent();
+		extent.left = left;
+		extent.top = top;
+		extent.width = width;
+		extent.height = height;
+		return extent;
 	}
 
 	/**
-	 * Returns whether this extent, considered as a closed set of points 
-	 * (i.e. including its edges and its vertices), contains the given point.
+	 * Returns whether this extent, considered as a closed set of points (i.e.
+	 * including its edges and its vertices), contains the given point.
+	 * 
 	 * @param point
 	 * @return
 	 */
 	public boolean contains(IntPoint point) {
-		return this.contains(point);
+		RoundedPolygon polygon = new RoundedPolygon();
+		IntPoint[] vertices = new IntPoint[] { new IntPoint(this.left, this.top),
+				new IntPoint(this.left + this.width, this.top),
+				new IntPoint(this.left + this.width, this.top + this.height),
+				new IntPoint(this.left, this.top + this.height) };
+		polygon.setVertices(vertices);
+		return polygon.contains(point);
 	}
-	
+
 	/**
-	 * Returns the Y coordinate of the edge parallel to the X axis with the largest Y coordinate.
+	 * Returns the Y coordinate of the edge parallel to the X axis with the largest
+	 * Y coordinate.
+	 * 
 	 * @return
 	 */
 	public int getBottom() {
-		return this.getTop()-this.getHeight();
+		return this.getTop() + this.getHeight();
 	}
-	
+
 	public IntPoint getBottomRight() {
-		int right = this.getLeft() + this.getWidth();
-		int bottom = this.getTop() - this.getBottom();
+		int right = this.getRight();
+		int bottom = this.getBottom();
 		IntPoint bottomright = new IntPoint(right, bottom);
 		return bottomright;
 	}
-	
+
 	/**
 	 * Returns the distance between the edges that are parallel to the X axis.
+	 * 
 	 * @return
 	 */
 	public int getHeight() {
 		return this.height;
 	}
-	
+
 	/**
-	 * Returns the X coordinate of the edge parallel to the Y axis with the smallest X coordinate.
+	 * Returns the X coordinate of the edge parallel to the Y axis with the smallest
+	 * X coordinate.
+	 * 
 	 * @return
 	 */
 	public int getLeft() {
 		return this.left;
 	}
-	
+
 	/**
-	 * Returns the X coordinate of the edge parallel to the Y axis with the largest X coordinate.
+	 * Returns the X coordinate of the edge parallel to the Y axis with the largest
+	 * X coordinate.
+	 * 
 	 * @return
 	 */
 	public int getRight() {
-		return this.left+this.width;
+		return this.left + this.width;
 	}
-	
+
 	/**
-	 * Returns the Y coordinate of the edge parallel to the X axis with the smallest Y coordinate.
+	 * Returns the Y coordinate of the edge parallel to the X axis with the smallest
+	 * Y coordinate.
+	 * 
 	 * @return
 	 */
 	public int getTop() {
 		return this.top;
 	}
-	
+
 	public IntPoint getTopLeft() {
 		IntPoint topleft = new IntPoint(this.getLeft(), this.getTop());
 		return topleft;
 	}
-	
+
 	/**
 	 * Returns the distance between the edges that are parallel to the Y axis.
+	 * 
 	 * @return
 	 */
 	public int getWidth() {
 		return this.width;
 	}
-	
-	public static Extent ofLeftTopRightBottom(int left, int top, int right, int bottom) {
-		Extent shape = new Extent(left, top, right, bottom);
-		return shape;
-	}
-	
-	public static Extent ofLeftTopWidthHeight(int left, int top, int width, int height) {
-		int right = left+width;
-		int bottom = top-height;
-		Extent newRectangle = new Extent(left, top, right, bottom);
-		return newRectangle;
-	}
-	
+
 	/**
-	 * Returns an object that has the given bottom coordinate and the same left, top, and right coordinate as this object.
+	 * Returns an object that has the given bottom coordinate and the same left,
+	 * top, and right coordinate as this object.
+	 * 
 	 * @param newBottom
 	 * @return
 	 */
 	public Extent withBottom(int newBottom) {
-		Extent newextent = new Extent(this.getLeft(), this.getTop(), this.getRight(), newBottom);
-		return newextent;
+		Extent newExtent = Extent.ofLeftTopRightBottom(this.getLeft(), this.getTop(), this.getRight(), newBottom);
+		return newExtent;
 	}
-	
+
 	/**
-	 * Returns an object that has the given height and the same left, top, and right coordinate as this object.
-	 * @param NewHeight
-	 * @return
-	 */
-	public Extent withHeight(int NewHeight) {
-		int newBottom = this.getTop()-NewHeight;
-		Extent newextent = new Extent(this.getLeft(), this.getTop(), this.getRight(), newBottom);
-		return newextent;
-	}
-	
-	/**
-	 * Returns an object that has the given left coordinate and the same right, top, and bottom coordinate as this object.
-	 * @param newLeft
-	 * @return
-	 */
-	public Extent withLeft(int newLeft) {
-		Extent newextent = new Extent(newLeft, this.getTop(), this.getRight(), this.getBottom());
-		return newextent;
-	}
-	
-	/**
-	 * Returns an object that has the given right coordinate and the same left, top, and bottom coordinate as this object.
+	 * Returns an object that has the given right coordinate and the same left, top,
+	 * and bottom coordinate as this object.
+	 * 
 	 * @param newRight
 	 * @return
 	 */
 	public Extent withRight(int newRight) {
-		Extent newextent = new Extent(this.getLeft(), this.getTop(), newRight, this.getBottom());
-		return newextent;
+		Extent newExtent = Extent.ofLeftTopRightBottom(this.getLeft(), this.getTop(), newRight, this.getBottom());
+		return newExtent;
 	}
-	
+
 	/**
-	 * Returns an object that has the given top coordinate and the same left, right, and bottom coordinate as this object.
+	 * Returns an object that has the given left coordinate and the same right, top,
+	 * and bottom coordinate as this object.
+	 * 
+	 * @param newLeft
+	 * @return
+	 */
+	public Extent withLeft(int newLeft) {
+		Extent newExtent = Extent.ofLeftTopRightBottom(newLeft, this.getTop(), this.getRight(), this.getBottom());
+		return newExtent;
+	}
+
+	/**
+	 * Returns an object that has the given top coordinate and the same left, right,
+	 * and bottom coordinate as this object.
+	 * 
 	 * @param newTop
 	 * @return
 	 */
 	public Extent withTop(int newTop) {
-		Extent newextent = new Extent(this.getLeft(), newTop, this.getRight(), this.getBottom());
-		return newextent;
+		Extent newExtent = Extent.ofLeftTopRightBottom(this.getLeft(), newTop, this.getRight(), this.getBottom());
+		return newExtent;
 	}
-	
+
 	/**
-	 * Returns an object that has the given width and the same left, top, and bottom coordinate as this object.
+	 * Returns an object that has the given height and the same left, top, and right
+	 * coordinate as this object.
+	 * 
+	 * @param NewHeight
+	 * @return
+	 */
+	public Extent withHeight(int newHeight) {
+		Extent newExtent = Extent.ofLeftTopWidthHeight(this.getLeft(), this.getTop(), this.getWidth(), newHeight);
+		return newExtent;
+	}
+
+	/**
+	 * Returns an object that has the given width and the same left, top, and bottom
+	 * coordinate as this object.
+	 * 
 	 * @param newWidth
 	 * @return
 	 */
 	public Extent withWidth(int newWidth) {
-		int newRight = left+newWidth;
-		Extent newextent = new Extent(this.getLeft(), this.getTop(), newRight, this.getBottom());
-		return newextent;
+		Extent newExtent = Extent.ofLeftTopWidthHeight(this.getLeft(), this.getTop(), newWidth, this.getHeight());
+		return newExtent;
 	}
-	
+
 }
--- drawit/src/drawit/shapegroups2/ShapeGroup.java
+++ drawit/src/drawit/shapegroups2/ShapeGroup.java
@@ -1,13 +1,23 @@
 package drawit.shapegroups2;
+
 import drawit.RoundedPolygon;
+
 public class ShapeGroup {
-	
-	
+	RoundedPolygon shape;
+	ShapeGroup[] subgroups;
+
 	public ShapeGroup(RoundedPolygon shape) {
+		this.shape = shape;
+
+	}
+
+	public ShapeGroup(ShapeGroup[] subgroups) {
+		this.subgroups = subgroups;
+	}
+	
+	public Extent getExtent() {
+		Extent extent = new Extent();
 		
 	}
 	
-	public ShapeGroup(ShapeGroup[] subgroups) {
-		
-	}
 }
