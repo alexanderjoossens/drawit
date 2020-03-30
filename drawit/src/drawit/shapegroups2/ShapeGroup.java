fe3ba273855e0bd43af1c689a7f38b0139f7fe8f
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-03-30 11:04:26 +0200

    alle functies al gedefinieerd, maar moet ze nog maken

--- drawit/src/drawit/shapegroups2/ShapeGroup.java
+++ drawit/src/drawit/shapegroups2/ShapeGroup.java
@@ -1,5 +1,7 @@
 package drawit.shapegroups2;
 
+import drawit.IntPoint;
+import drawit.IntVector;
 import drawit.RoundedPolygon;
 
 public class ShapeGroup {
@@ -15,9 +17,66 @@ public class ShapeGroup {
 		this.subgroups = subgroups;
 	}
 	
+	
+	
 	public Extent getExtent() {
-		Extent extent = new Extent();
+		return null;
 		
 	}
 	
+	public Extent getOriginalExtent() {
+		return null;
+	}
+	
+	public IntPoint toGLobalCoordinates (IntPoint innerCoordinates) {
+		return null;
+	}
+	
+	public IntPoint toInnerCoordinates(IntPoint globalCoordinates) {
+		return null;
+	}
+	
+	public IntVector toInnerCoordinates(IntVector relativeGlobalCoordinates) {
+		return null;
+	}
+	
+	public void setExtent(Extent newExtent) {
+		
+	}
+	
+	public RoundedPolygon getShape() {
+		return null;
+	}
+	
+	public ShapeGroup getSubgroup(int index) {
+		return null;
+	}
+	
+	public ShapeGroup getParentgroup() {
+		return null;
+	}
+	
+	public java.util.List<ShapeGroup> getSubgroups() {
+		return null;
+	}
+	
+	public int getSubgroupCount() {
+		return 0;
+	}
+	
+	public ShapeGroup getSubgroupAt(IntPoint innerCoordinates) {
+		return null;
+	}
+	
+	public void bringToFront() {
+		
+	}
+	
+	public void sendToBack() {
+		
+	}
+	
+	public java.lang.String getDrawingCommands() {
+		return null;
+	}
 }
