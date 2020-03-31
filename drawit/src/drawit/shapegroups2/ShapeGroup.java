f916ac5ceea153768f22f978d5fb5f9441dc8a56
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-03-31 12:48:04 +0200

    getExtent recursief geimplementeerd

--- drawit/src/drawit/shapegroups2/ShapeGroup.java
+++ drawit/src/drawit/shapegroups2/ShapeGroup.java
@@ -5,8 +5,10 @@ import drawit.IntVector;
 import drawit.RoundedPolygon;
 
 public class ShapeGroup {
-	RoundedPolygon shape;
-	ShapeGroup[] subgroups;
+	private RoundedPolygon shape;
+	private ShapeGroup[] subgroups;
+	private float horizontalScale;
+	private float verticalScale;
 
 	public ShapeGroup(RoundedPolygon shape) {
 		this.shape = shape;
@@ -16,95 +18,111 @@ public class ShapeGroup {
 	public ShapeGroup(ShapeGroup[] subgroups) {
 		this.subgroups = subgroups;
 	}
-	
-	
-	
+
 	public Extent getExtent() {
 		int maxX = 0;
 		int maxY = 0;
 		int minX = 0;
 		int minY = 0;
-		IntPoint[] points = shape.getVertices();
-		for (int i = 0 ; i<points.length; i++) {
-			int xCoord = points[i].getX();
-			int yCoord = points[i].getY();
-			if (i ==0) {
-				maxX = xCoord;
-				minX = xCoord;
-				maxY = yCoord;
-				minY = yCoord;
-			}
-			else {
-				maxX = Math.max(maxX, xCoord);
-				maxY = Math.max(maxY, yCoord);
-				minX = Math.min(minX, xCoord);
-				minY = Math.min(minY, yCoord);
-
+		if (shape==null) {
+			for(ShapeGroup shapeGroup: this.getSubgroups()) {
+				Extent extent = shapeGroup.getExtent();
+				if (maxX==0 && maxY == 0 && minX == 0 && minY ==0) {
+					maxY = extent.getBottom();
+					maxX = extent.getRight();
+					minY = extent.getTop();
+					minX = extent.getLeft();
+				}
+				else {
+					maxY = Math.max(extent.getBottom(),maxY);
+					maxX = Math.max(extent.getRight(),maxX);
+					minY = Math.min(extent.getTop(),minY);
+					minX = Math.min(extent.getLeft(),minX);
+				}
 				
 			}
 		}
+		
+		else {
+			IntPoint[] points = shape.getVertices();
+			for (int i = 0; i < points.length; i++) {
+				int xCoord = points[i].getX();
+				int yCoord = points[i].getY();
+				if (i == 0) {
+					maxX = xCoord;
+					minX = xCoord;
+					maxY = yCoord;
+					minY = yCoord;
+				} else {
+					maxX = Math.max(maxX, xCoord);
+					maxY = Math.max(maxY, yCoord);
+					minX = Math.min(minX, xCoord);
+					minY = Math.min(minY, yCoord);
 	
-		Extent extent = Extent.ofLeftTopRightBottom(minX,minY,maxX,maxY);
+				}
+			}
+		}
+
+		Extent extent = Extent.ofLeftTopRightBottom(minX, minY, maxX, maxY);
 		return extent;
 	}
-	
+
 	public Extent getOriginalExtent() {
 		return null;
 	}
-	
-	public IntPoint toGLobalCoordinates (IntPoint innerCoordinates) {
+
+	public IntPoint toGLobalCoordinates(IntPoint innerCoordinates) {
 		return null;
 	}
-	
+
 	public IntPoint toInnerCoordinates(IntPoint globalCoordinates) {
 		return null;
 	}
-	
+
 	public IntVector toInnerCoordinates(IntVector relativeGlobalCoordinates) {
 		return null;
 	}
-	
+
 	public void setExtent(Extent newExtent) {
-		
+
 	}
-	
+
 	public RoundedPolygon getShape() {
 		if (shape != null) {
 			return shape;
-		}
-		else {
+		} else {
 			return null;
 		}
 	}
-	
+
 	public ShapeGroup getParentgroup() {
 		return null;
 	}
-	
+
 	public ShapeGroup[] getSubgroups() {
 		return subgroups;
 	}
-	
+
 	public int getSubgroupCount() {
 		return this.getSubgroups().length;
 	}
-	
+
 	public ShapeGroup getSubgroup(int index) {
 		return this.getSubgroups()[index];
 	}
-	
+
 	public ShapeGroup getSubgroupAt(IntPoint innerCoordinates) {
 		return null;
 	}
-	
+
 	public void bringToFront() {
-		
+
 	}
-	
+
 	public void sendToBack() {
-		
+
 	}
-	
+
 	public java.lang.String getDrawingCommands() {
 		return null;
 	}
