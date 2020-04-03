fd867d8eb2a1c857db3fa3a9b789e8e7589a5109
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-04-03 16:18:29 +0200

    wat gewerkt aan shapegroups2

--- drawit/src/drawit/shapegroups2/ShapeGroup.java
+++ drawit/src/drawit/shapegroups2/ShapeGroup.java
@@ -13,66 +13,73 @@ public class ShapeGroup {
 	
 	
 
-	public ShapeGroup(RoundedPolygon shape) {
-		this.shape = shape;
-		this.originalExtent = this.getExtent();
-
-	}
-
-	public ShapeGroup(ShapeGroup[] subgroups) {
-		this.subgroups = subgroups;
-		this.originalExtent = this.getExtent();
-		for (ShapeGroup shapeGroup: subgroups) {
-			shapeGroup.parentGroup = this;
-		}
-	}
-
-	public Extent getExtent() {
+	public ShapeGroup(RoundedPolygon shape) {		
 		int maxX = 0;
 		int maxY = 0;
 		int minX = 0;
 		int minY = 0;
-		if (shape==null) {
-			for(ShapeGroup shapeGroup: this.getSubgroups()) {
-				Extent extent = shapeGroup.getExtent();
-				if (maxX==0 && maxY == 0 && minX == 0 && minY ==0) {
-					maxY = extent.getBottom();
-					maxX = extent.getRight();
-					minY = extent.getTop();
-					minX = extent.getLeft();
-				}
-				else {
-					maxY = Math.max(extent.getBottom(),maxY);
-					maxX = Math.max(extent.getRight(),maxX);
-					minY = Math.min(extent.getTop(),minY);
-					minX = Math.min(extent.getLeft(),minX);
-				}
-				
+		
+		IntPoint[] points = shape.getVertices();
+		for (int i = 0; i < points.length; i++) {
+			int xCoord = points[i].getX();
+			int yCoord = points[i].getY();
+			if (i == 0) {
+				maxX = xCoord;
+				minX = xCoord;
+				maxY = yCoord;
+				minY = yCoord;
+			} else {
+				maxX = Math.max(maxX, xCoord);
+				maxY = Math.max(maxY, yCoord);
+				minX = Math.min(minX, xCoord);
+				minY = Math.min(minY, yCoord);
 			}
 		}
 		
-		else {
-			IntPoint[] points = shape.getVertices();
-			for (int i = 0; i < points.length; i++) {
-				int xCoord = points[i].getX();
-				int yCoord = points[i].getY();
-				if (i == 0) {
-					maxX = xCoord;
-					minX = xCoord;
-					maxY = yCoord;
-					minY = yCoord;
-				} else {
-					maxX = Math.max(maxX, xCoord);
-					maxY = Math.max(maxY, yCoord);
-					minX = Math.min(minX, xCoord);
-					minY = Math.min(minY, yCoord);
-	
-				}
-			}
-		}
-
 		Extent extent = Extent.ofLeftTopRightBottom(minX, minY, maxX, maxY);
-		return extent;
+		this.originalExtent = extent;
+		this.ownExtent = extent;
+
+	}
+
+	public ShapeGroup(ShapeGroup[] subgroups) {
+		for (ShapeGroup shapeGroup: subgroups) {
+			shapeGroup.parentGroup = this;
+		}
+		
+		
+		int maxX = 0;
+		int maxY = 0;
+		int minX = 0;
+		int minY = 0;
+		for(ShapeGroup shapeGroup: subgroups) {
+			Extent extent = shapeGroup.getExtent();
+			if (maxX==0 && maxY == 0 && minX == 0 && minY ==0) {
+				maxY = extent.getBottom();
+				maxX = extent.getRight();
+				minY = extent.getTop();
+				minX = extent.getLeft();
+			}
+			else {
+				maxY = Math.max(extent.getBottom(),maxY);
+				maxX = Math.max(extent.getRight(),maxX);
+				minY = Math.min(extent.getTop(),minY);
+				minX = Math.min(extent.getLeft(),minX);
+			}
+			
+		}
+		
+		Extent extent = Extent.ofLeftTopRightBottom(minX, minY, maxX, maxY);
+		this.originalExtent = extent;
+		this.ownExtent = extent;
+	}
+	
+	public void setExtent(Extent newExtent) {
+		this.ownExtent = newExtent;
+	}
+
+	public Extent getExtent() {
+		return ownExtent;
 	}
 
 	public Extent getOriginalExtent() {
@@ -91,9 +98,6 @@ public class ShapeGroup {
 //		return null;
 //	}
 
-	public void setExtent(Extent newExtent) {
-		this.ownExtent = newExtent;
-	}
 
 	public RoundedPolygon getShape() {
 		if (shape != null) {
@@ -135,3 +139,73 @@ public class ShapeGroup {
 //		return null;
 //	}
 }
+
+
+
+
+
+
+
+// Dit is oude code, maar ik hou het bij voor als nieuwe code niet werkt
+
+//public ShapeGroup(RoundedPolygon shape) {
+//	this.shape = shape;
+//	this.originalExtent = this.getExtent();
+//
+//}
+//
+//public ShapeGroup(ShapeGroup[] subgroups) {
+//	this.subgroups = subgroups;
+//	this.originalExtent = this.getExtent();
+//	for (ShapeGroup shapeGroup: subgroups) {
+//		shapeGroup.parentGroup = this;
+//	}
+//}
+//
+//public Extent getExtent() {
+//	int maxX = 0;
+//	int maxY = 0;
+//	int minX = 0;
+//	int minY = 0;
+//	if (shape==null) {
+//		for(ShapeGroup shapeGroup: this.getSubgroups()) {
+//			Extent extent = shapeGroup.getExtent();
+//			if (maxX==0 && maxY == 0 && minX == 0 && minY ==0) {
+//				maxY = extent.getBottom();
+//				maxX = extent.getRight();
+//				minY = extent.getTop();
+//				minX = extent.getLeft();
+//			}
+//			else {
+//				maxY = Math.max(extent.getBottom(),maxY);
+//				maxX = Math.max(extent.getRight(),maxX);
+//				minY = Math.min(extent.getTop(),minY);
+//				minX = Math.min(extent.getLeft(),minX);
+//			}
+//			
+//		}
+//	}
+//	
+//	else {
+//		IntPoint[] points = shape.getVertices();
+//		for (int i = 0; i < points.length; i++) {
+//			int xCoord = points[i].getX();
+//			int yCoord = points[i].getY();
+//			if (i == 0) {
+//				maxX = xCoord;
+//				minX = xCoord;
+//				maxY = yCoord;
+//				minY = yCoord;
+//			} else {
+//				maxX = Math.max(maxX, xCoord);
+//				maxY = Math.max(maxY, yCoord);
+//				minX = Math.min(minX, xCoord);
+//				minY = Math.min(minY, yCoord);
+//
+//			}
+//		}
+//	}
+//
+//	Extent extent = Extent.ofLeftTopRightBottom(minX, minY, maxX, maxY);
+//	return extent;
+//}
