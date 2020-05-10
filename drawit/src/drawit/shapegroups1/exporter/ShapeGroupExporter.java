dd1f304f987c6316bf3e9d6fc124adc4ca715cb2
alexander <alexanderjoossens@gmail.com>
2020-05-10 14:54:22 +0200

    nog veel gewerkt aan deel 3

--- drawit/src/drawit/shapegroups1/exporter/ShapeGroupExporter.java
+++ drawit/src/drawit/shapegroups1/exporter/ShapeGroupExporter.java
@@ -4,37 +4,52 @@ import java.util.List;
 import java.util.Map;
 import drawit.shapegroups1.*;
 
-import drawit.shapegroups1.ShapeGroup;
-
 public class ShapeGroupExporter {
 
-	// recursieve functie, moet van alle extents van leafgroups de coordinaten printen
+	// recursieve functie
 	public static Object toPlainData(ShapeGroup shapeGroup) {
 		
 		// base case: leaf group
 		if (shapeGroup.getParentGroup() == null) {
 			
 			String result = "";
-			for (int i; shapeGroup.getVertices(); i++)
-				result += Map.of("x", getVertices()[i].getX,
-								 "y", getVertices()[i].getY);
+			result += "originalExtent" + Map.of(
+					  		"left", shapeGroup.getOriginalExtent().getLeft(), 
+					  		"top", shapeGroup.getOriginalExtent().getTop(),
+					  		"right", shapeGroup.getOriginalExtent().getRight(),
+					  		"bottom", shapeGroup.getOriginalExtent().getBottom());
+			result += "extent" + Map.of("left", shapeGroup.getExtent().getLeft(), 
+			        		 			"top", shapeGroup.getExtent().getTop(),
+			        		 			"right", shapeGroup.getExtent().getRight(),
+			        		 			"bottom", shapeGroup.getExtent().getBottom());
+			result += "shape";
+			for (int i; ((LeafShapeGroup)shapeGroup).getShape().getVertices()[i] != null; i++)
+					result += Map.of("x", ((LeafShapeGroup)shapeGroup).getShape().getVertices()[i].getX(),
+									 "y", ((LeafShapeGroup)shapeGroup).getShape().getVertices()[i].getY());
 		
-		return result;
+			result += "radius" + ((LeafShapeGroup)shapeGroup).getRadius();
+			result += "color" + ((LeafShapeGroup)shapeGroup).getColor();
+			
+			return result;
 				
 		}
 		
-		//recursieve oproep voor 1 verder in de boom
-		return Map.of(
-				"originalExtent", Map.of(
-						"left", shapeGroup.getOriginalExtent().getLeft(), 
-						"top", shapeGroup.getOriginalExtent().getTop(),
-						"right", shapeGroup.getOriginalExtent().getRight(),
-						"bottom", shapeGroup.getOriginalExtent().getBottom())
-				, "extent", Map.of(	"left", shapeGroup.getOriginalExtent().getLeft(), 
-									"top", shapeGroup.getOriginalExtent().getTop(),
-									"right", shapeGroup.getOriginalExtent().getRight(),
-									"bottom", shapeGroup.getOriginalExtent().getBottom())
-				, "subgroups", List.of(shapeGroup.getParentGroup().toPlainData());
+		//recursieve oproep voor 1 nonleafgroup verder in de boom
+		String result = "";
+		result += "originalExtent" + Map.of(
+				  		"left", shapeGroup.getOriginalExtent().getLeft(), 
+				  		"top", shapeGroup.getOriginalExtent().getTop(),
+				  		"right", shapeGroup.getOriginalExtent().getRight(),
+				  		"bottom", shapeGroup.getOriginalExtent().getBottom());
+		result += "extent" + Map.of("left", shapeGroup.getExtent().getLeft(), 
+		        		 			"top", shapeGroup.getExtent().getTop(),
+		        		 			"right", shapeGroup.getExtent().getRight(),
+		        		 			"bottom", shapeGroup.getExtent().getBottom());
+		result += "subgroups";
+		for (int i; ((NonleafShapeGroup)shapeGroup).getSubgroup(i) != null; i++)
+				result += toPlainData(((NonleafShapeGroup)shapeGroup).getSubgroup(i));
+		
+		return result;
 				
 	}
 	
