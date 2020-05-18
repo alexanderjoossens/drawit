25d37cfa4713376091851f9ce5d2cd0c079572ae
Stefan Lommaert <stefan.lommaert@student.kuleuven.be>
2020-05-18 22:35:19 +0200

    doc geschreven

--- drawit/src/drawit/FastRoundedPolygonContainsTestStrategy.java
+++ drawit/src/drawit/FastRoundedPolygonContainsTestStrategy.java
@@ -3,6 +3,11 @@ package drawit;
 public class FastRoundedPolygonContainsTestStrategy implements RoundedPolygonContainsTestStrategy {
 
 	@Override
+	/**
+	 * @pre | polygon != null
+	 * @pre | point != null
+	 * @post | result == polygon.getBoundingBox().contains(point)
+	 */
 	public boolean contains(RoundedPolygon polygon, IntPoint point) {
 		return polygon.getBoundingBox().contains(point);
 	}
--- drawit/src/drawit/PreciseRoundedPolygonContainsTestStrategy.java
+++ drawit/src/drawit/PreciseRoundedPolygonContainsTestStrategy.java
@@ -3,6 +3,11 @@ package drawit;
 public class PreciseRoundedPolygonContainsTestStrategy implements RoundedPolygonContainsTestStrategy {
 
 	@Override
+	/**
+	 * @pre | polygon != null
+	 * @pre | point != null
+	 * @post | result == polygon.contains(point)
+	 */
 	public boolean contains(RoundedPolygon polygon, IntPoint point) {
 		return polygon.contains(point);
 	}
